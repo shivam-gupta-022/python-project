5cf183469322659a0985cb6d62362a680bc5763e
shivam-gupta-022 <102383206+shivam-gupta-022@users.noreply.github.com>
2022-05-30 10:20:13 +0530

    Create print_numbers_greaterorequal_to _a_given_num.py

---
+++ numpy/print_numbers_greaterorequal_to _a_given_num.py
@@ -0,0 +1,13 @@
+#NumPy program to print all numbers from a given array which are equal and greater than a specified number.
+import numpy as np  
+nums = np.array([[5.54, 3.38, 7.99],
+              [3.54, 4.38, 6.99],
+              [1.54, 2.39, 9.29]])
+print("Original array:")
+print(nums)
+n = 5
+print("\nElements of the said array greater than",n)
+print(nums[nums > n])
+n = 6
+print("\nElements of the said array equal to",n)
+print(nums[nums == n])
